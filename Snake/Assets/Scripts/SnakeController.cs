32b8d8e59b92fca7b110737a2ebb454682b8ca55
moose10141 <moose10141@gmail.com>
2019-06-16 22:22:53 +1000

    Added Food and Windows Build
    Snake can now eat and grow

--- Snake/Assets/Scripts/SnakeController.cs
+++ Snake/Assets/Scripts/SnakeController.cs
@@ -6,7 +6,9 @@ public class SnakeController : MonoBehaviour {
 
     private int speed = 40;
     private int frames = 0;
+    public int startingLength = 3;
     private bool canTurn = true;
+    public GameObject bodyPrefab;
     List<GameObject> body = new List<GameObject>();
 
 	// Use this for initialization
@@ -18,7 +20,7 @@ public class SnakeController : MonoBehaviour {
         {
             body.Add(bodyPart);
         }
-        setUpSnake();
+        setUpSnake(startingLength);
     }
 
     void FixedUpdate()
@@ -36,7 +38,7 @@ public class SnakeController : MonoBehaviour {
         float input = Input.GetAxisRaw("Horizontal");
         if (canTurn == true && input != 0)
         {
-            transform.localEulerAngles += new Vector3(0, input * 90, 0);
+            transform.eulerAngles += new Vector3(0, input * 90, 0);
             canTurn = false;
         }
     }
@@ -54,11 +56,42 @@ public class SnakeController : MonoBehaviour {
         }
     }
 
-    private void setUpSnake()
+    private void setUpSnake(int startingLength)
     {
+        for (int i = 0; i < startingLength; i++)
+        {
+            body.Add(Instantiate(bodyPrefab, body[i].transform.position - body[i].transform.forward, body[i].transform.rotation));
+        }
+
         for (int i = 1; i < body.Count; i++)
         {
             body[i].transform.position = body[i - 1].transform.position - body[i - 1].transform.forward;
         }
+    }
+
+    private void extendBody()
+    {
+        body.Add(Instantiate(bodyPrefab, body[body.Count - 1].transform.position, body[body.Count - 1].transform.rotation));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            extendBody();
+            List<Vector3> takenPositions = new List<Vector3>();
+            foreach (GameObject bodyPart in body)
+            {
+                takenPositions.Add(bodyPart.transform.position);
+            }
+            Vector3 newPos;
+            do
+            {
+                newPos = new Vector3(Mathf.Round(Random.Range(-8, 8)), other.transform.position.y, Mathf.Round(Random.Range(-4, 4)));
+            }
+            while (takenPositions.Contains(newPos));
+            
+            other.transform.position = newPos;
+        } 
     }
 }
